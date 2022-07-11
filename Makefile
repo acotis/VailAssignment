ba74ed3c9762fd3489346cedb0a8b057b0cd2dfb
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2022-07-11 11:06:28 -0700

    Added two tests and improved the structure of the testing suite method.

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@
 all:
 	@python3 main.py
 
+push:
+	@git push https://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@github.com/acotis/VailAssignment.git
+
--- main.py
+++ main.py
@@ -13,6 +13,28 @@ def rotate_array(array, shift_amount):
 def pad_string(string, length):
     return string + " " * (length - len(string))
 
+# Generate a pretty report for a single test case of the string rotation method.
+
+def create_report(array, shift_amount, expected_output, alt_name = None):
+    array_col_width = 20
+    shift_col_width = 10
+    test_pass_string = "\033[92mOK\033[0m"        # String "OK" in green text
+    test_fail_string = "\033[91m\033[1m{}\033[0m" # Actual output in bold red text
+
+    actual_output = rotate_array(array, shift_amount)
+    test_name = str(array) if alt_name == None else alt_name
+
+    report = ""
+    report += pad_string(test_name, array_col_width)
+    report += pad_string(str(shift_amount), shift_col_width)
+        
+    if actual_output == expected_output:
+        report += test_pass_string
+    else:
+        report += test_fail_string.format(actual_output)
+
+    return report
+
 # Put in a series of calls to the array_rotator method and print a report of
 # whether it gets the right answers.
 
@@ -29,31 +51,27 @@ def test_array_rotator():
              ([],               0,                  []),
              ([9, 8, 1],        1000000,            [8, 1, 9])]
 
-    # Todo: add these "big list" tests which require special pretty-printing
-
-    # long_list = [i for i in range(100000)]
-    # long_list_rotate_right_one = [99999] + [i for i in range(99999)]
-             # (long_list,        0,                  long_list),
-             # (long_list         len(long_list)-1,   long_list_rotate_right_one)]
 
     # todo: also create some tests that ensure an exception is raised when the
     # shift amount is negative or not an integer or when other stuff is weird
 
+    # Run the normal tests, and pretty-print the results.
+
     for array, shift_amount, expected_output in tests:
-        actual_output = rotate_array(array, shift_amount)
+        print(create_report(array, shift_amount, expected_output))
 
-        output = ""
-        output += pad_string(str(array), 20)
-        output += pad_string(str(shift_amount), 10)
-        
-        if actual_output == expected_output:
-            # String "OK" printed in green text
-            output += "\033[92mOK\033[0m"
-        else:
-            # Incorrect output printed in bold red text
-            output += "\033[91m\033[1m" + str(actual_output) + "\033[0m"
+    # Run two tests on a very large input array, and pretty-print the results.
+    # These tests are separated because they require special pretty-printing
 
-        print(output)
 
-        
+    # Todo: add these "big list" tests which require special pretty-printing
+
+    long_list = [i for i in range(100000)]
+    long_list_rotate_right_one = [99999] + [i for i in range(99999)]
+
+    print(create_report(long_list, 0,                long_list, alt_name = "<long_list>"))
+    print(create_report(long_list, len(long_list)-1, long_list, alt_name = "<long_list>"))
+
+
 test_array_rotator()
+
