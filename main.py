7cec59871125e40c8ccb1f643bfdc90c4d61aba0
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2022-07-11 11:23:22 -0700

    Added two tests for invalid inputs.

--- main.py
+++ main.py
@@ -7,19 +7,24 @@
 def rotate_array(array, shift_amount):
     return []
 
-# Pad out a string with spaces to achieve a specified length (used for pretty-printing
-# by the test_array_rotator() method).
+
+# Pad out a string with spaces to achieve a specified length.
 
 def pad_string(string, length):
     return string + " " * (length - len(string))
+
+def make_string_green(string):
+    return "\033[92m{}\033[0m".format(string)
+
+def make_string_bold_red(string):
+    return "\033[91m\033[1m{}\033[0m".format(string)
+
 
 # Generate a pretty report for a single test case of the string rotation method.
 
 def create_report(array, shift_amount, expected_output, alt_name = None):
     array_col_width = 20
     shift_col_width = 10
-    test_pass_string = "\033[92mOK\033[0m"        # String "OK" in green text
-    test_fail_string = "\033[91m\033[1m{}\033[0m" # Actual output in bold red text
 
     actual_output = rotate_array(array, shift_amount)
     test_name = str(array) if alt_name == None else alt_name
@@ -29,11 +34,12 @@ def create_report(array, shift_amount, expected_output, alt_name = None):
     report += pad_string(str(shift_amount), shift_col_width)
         
     if actual_output == expected_output:
-        report += test_pass_string
+        report += make_string_green("OK")
     else:
-        report += test_fail_string.format(actual_output)
+        report += make_string_bold_red(str(actual_output))
 
     return report
+
 
 # Put in a series of calls to the array_rotator method and print a report of
 # whether it gets the right answers.
@@ -59,8 +65,26 @@ def test_array_rotator():
     print(create_report(long_list, 0,                long_list,       alt_name = "<long_list>"))
     print(create_report(long_list, len(long_list)-1, long_list_shift, alt_name = "<long_list>"))
 
-    # todo: also create some tests that ensure an exception is raised when the
-    # shift amount is negative or not an integer or when other stuff is weird
+    # Out-of-bounds tests for negative and non-integer shift amounts.
+
+    try:
+        output = rotate_array([1, 2, 3], -1)
+    except ValueError:
+        print(make_string_green("Negative shift amount raises ValueError."))
+    except:
+        print(make_string_bold_red("Negative shift amount raises wrong kind of exception."))
+    else:
+        print(make_string_bold_red("Negative shift amount does not raise an error."))
+
+    try:
+        output = rotate_array([1, 2, 3], 0.5)
+    except ValueError:
+        print(make_string_green("Non-integer shift amount raises ValueError."))
+    except:
+        print(make_string_bold_red("Non-integer shift amount raises wrong kind of exception."))
+    else:
+        print(make_string_bold_red("Non-integer shift amount does not raise an error."))
+
 
 test_array_rotator()
 
